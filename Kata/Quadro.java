0babc8509aa575673ade9168bc5f8f684a60be3c
programmer-maksimus <maksimdavydenko12@gmail.com>
2023-06-23 12:57:16 +0700

    first solution

--- Kata/Quadro.java
+++ Kata/Quadro.java
@@ -2,6 +2,9 @@ package Kata;
 
 public class Quadro {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println(quadrant(1,2));
+    }
+    public static int quadrant(int x, int y) {
+        return x > 0 ? y > 0 ? 1 : 4 : y > 0 ? 2 : 3;
     }
 }

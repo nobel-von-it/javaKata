15153fc7718bf6387ad12b9c54a4b9c25c95a16c
progermaks <maksimdavydenko12@gmail.com>
2023-09-01 23:53:40 +0700

    01092023

--- Kata/SortTextbooks.java
+++ Kata/SortTextbooks.java
@@ -1,4 +1,11 @@
 package Kata;
 
+import java.util.List;
+
 public class SortTextbooks {
+    public static List<String > sort(List<String> textbooks) {
+        textbooks.sort(String.CASE_INSENSITIVE_ORDER);
+        return textbooks;
+//        return textbooks.stream().sorted(String.CASE_INSENSITIVE_ORDER).toList();
+    }
 }
